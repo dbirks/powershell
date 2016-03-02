5738fbd96c0012a77b34b40d6812f591468c9e30
David Birks <davidebirks@gmail.com>
2016-03-02 17:04:01 -0500

    enable built-in admin account, set screensaver

--- powershell/PrepComputer.ps1
+++ powershell/PrepComputer.ps1
@@ -1,3 +1,6 @@
+#disable offline files
+Set-Service -name "Offline Files" -StartupType Disabled -Status Stopped
+
 #disable system protection
 disable-computerrestore -drive "C:\"
 
@@ -7,11 +10,19 @@ Set-Service -name "Remote Registry" -StartupType Automatic -Status Running
 #set power plan to High Performance
 Powercfg -SETACTIVE SCHEME_MIN
 
-#disable offline files
-Set-Service -name "Offline Files" -StartupType Disabled -Status Stopped
-
 #create C:\Temp
 mkdir C:\Temp
 
 #disable firewall
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+
+#enable administrator account
+$Admin-Pass = Read-Host -Prompt 'Please enter the local admin password'
+net user administrator $Admin-Pass
+net user administrator /active
+
+#set mystify screensaver
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v SCRNSAVE.EXE /t REG_SZ /d C:\Windows\system32\Mystify.scr /f
+
+#set screensaver timeout to 30 minutes
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v ScreenSaveTimeOut /t REG_SZ /d 1800 /f
